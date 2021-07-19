c923ac92cf9f67de52185904599f8e6dc10387bc
parag56 <paragthakur56@gmail.com>
2021-07-19 23:10:44 +0530

    particlejs dependency fix

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import Particles from 'react-particles-js';
+import Particles from 'react-tsparticles';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import Navbar from '../Navbar';
@@ -10,41 +10,51 @@ import Chatbox from '../AdminSupport/Chatbox'
 // import Footer from './Footer';
 
 const HomePage = (props) => {
-    
     return (
         <div className="homepage">
            <Particles
         params={{
           particles: {
             number: {
-              value:60,
+              value:50,
               density: {
                 enable: true,
                 value_area: 1000,
               }
             },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            collisions: {
+              enable: true,
+            },
             "color": {
               "value": ["#BD10E0","#B8E986","#50E3C2","#FFD300","#E86363"]
             },
             "shape": {
-              "type": "square",
+              "type": "polygon",
               "stroke": {
                 "width": 0,
                 "color": "#b6b2b2"
               }
-            }
-            ,
+            },
+            
              "size": {
               "value": 8.017060304327615,
               "random": true,
               "anim": {
                 "enable": true,
-                "speed": 12.181158184520175,
+                "speed":6.181158184520175,
                 "size_min": 0.1,
                 "sync": true
               }
             },
-            "line_linked": {
+            lineLinked: {
               "enable": true,
               "distance": 150,
               "color": "#49c2b9",
@@ -52,14 +62,7 @@ const HomePage = (props) => {
               "width": 1
             }
            },
-           "interactivity": {
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": "repulse"
-                }
-            }
-        }
+           detectRetina: true,
   
         }}
       />
